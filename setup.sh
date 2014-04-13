c0ba5cc43266c8223c204bbe05da75e5f67fd988
andrew-l-o <andrew.otte@gmail.com>
2014-04-12 20:02:50 -0400

    cordova added to package.json and updated setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+WORKDIR=`pwd`
+CORDOVADIR=/tmp/cordova
 npm install
 bower install
 
-cd www/js/lib/crafty; npm install; grunt
+cd js/lib/crafty; npm install; grunt; cd $WORKDIR
+
+cordova create --link-to=$WORKDIR /tmp/cordova com.jroid.landwar LandWar; 
+
+cd $CORDOVADIR; cordova platform add android; cd $WORKDIR 
+mv $CORDOVADIR $WORKDIR
 
 
