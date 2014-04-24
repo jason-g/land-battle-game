c66a1c10818b4d07a67ae107a4c6970c883c96d5
andrew-l-o <andrew.otte@gmail.com>
2014-04-23 21:58:46 -0400

    Revert "cordova added to package.json and updated setup script"
    This reverts commit c0ba5cc43266c8223c204bbe05da75e5f67fd988.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-WORKDIR=`pwd`
-CORDOVADIR=/tmp/cordova
 npm install
 bower install
 
-cd js/lib/crafty; npm install; grunt; cd $WORKDIR
-
-cordova create --link-to=$WORKDIR /tmp/cordova com.jroid.landwar LandWar; 
-
-cd $CORDOVADIR; cordova platform add android; cd $WORKDIR 
-mv $CORDOVADIR $WORKDIR
+cd www/js/lib/crafty; npm install; grunt
 
 
